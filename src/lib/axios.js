ebec465ec551e0d7b6c10fd714af8e99737ff4f0
Olivier <oliviermorelle@hotmail.fr>
2023-01-17 18:58:58 +0100

    CORS Error

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,11 +1,35 @@
 import Axios from 'axios'
+import Cookies from 'js-cookie';
 
 const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
+        // 'Access-Control-Allow-Origin': '*',
+        'origin' : 'http://127.0.0.1:3000',
     },
     withCredentials: true,
 })
 
+/*
+const onRequest = (config) => {
+
+    if ((
+            config.method == 'post' || 
+            config.method == 'put' || 
+            config.method == 'delete'
+        ) &&
+        !Cookies.get('XSRF-TOKEN')) {
+        return setCSRFToken()
+            .then(response => config);
+    }
+    return config;
+}
+
+const setCSRFToken = () => {
+    return axios.get('/csrf-cookie'); 
+}
+
+axios.interceptors.request.use(onRequest, null);
+*/
 export default axios
--- src/pages/login.js
+++ src/pages/login.js
@@ -116,6 +116,13 @@ const Login = () => {
                     </div>
 
                     <div className="flex items-center justify-end mt-4">
+
+                        <Link
+                            href="/register"
+                            className="mx-4 text-sm text-gray-700 underline">
+                            Register
+                        </Link>
+                        
                         <Link
                             href="/forgot-password"
                             className="underline text-sm text-gray-600 hover:text-gray-900">
